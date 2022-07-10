b4f3b45aedff225029e2f948e48da52ff00802cb
Grace Guan <graceguan@Graces-MBP.hitronhub.home>
2022-07-10 11:48:59 -0700

    finished everything except navbar and spaces in between and footer

--- app.js
+++ app.js
@@ -52,3 +52,20 @@ setTimeout(loop, time)
 }
 
 loop()
+
+// NAVBAR
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+// contact
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
